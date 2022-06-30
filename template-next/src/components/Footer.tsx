f0b77bdec6b2b5dd282b6a92952b554dff8d065a
MacielSevilha <dev.macielsevilha@gmail.com>
2022-06-30 18:15:26 -0300

    zerei projeto

--- template-next/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/Footer.module.css'
-
-export default function Footer() {
-    return (
-        <div className='text-red'>
-            <p>Footer do template</p>
-        </div>
-    )
-}
