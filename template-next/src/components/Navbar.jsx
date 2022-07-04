64afb072958a736ce233c980a23ced5cbc19d4c7
MacielSevilha <dev.macielsevilha@gmail.com>
2022-07-03 22:31:52 -0300

    menu resolvido

--- template-next/src/components/Navbar.jsx
+++ template-next/src/components/Navbar.jsx
@@ -1,19 +1,33 @@
 import Link from "next/link"
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
  const Navbar = () => {
+
     const [nav, setNav] = useState(false)
-    const [color, setColor] = useState('transparent')
+    const [color, setColor] = useState('red')
     const [textColor, setTextColor] = useState('white')
 
     const handleNav = () => {
         setNav(!nav)
     }
+    
+    useEffect(() => {
+        const changeColor = () => {
+            if(window.scrollY >= 90) {
+                setColor('#FFFFFF')
+                setTextColor('#000000')
+            } else {
+                setColor('red')
+                setTextColor('#FFFFFF')
+            }
+        }
+        window.addEventListener('scroll', changeColor)
+    }, [])
 
 
     return (
-        <div  className="fixed left-0 top-0 w-full z-10 ease-in duration-300 bg-slate-600">
+        <div style={{background: `${color}`}} className="fixed left-0 top-0 w-full z-10 ease-in duration-300 bg-slate-600">
             <div className="max-w-[1240px] m-auto flex justify-between items-center p-4">
                 <ul className="hidden sm:flex">
                     <li className="">
@@ -33,12 +47,12 @@ import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
                     </li>
                 </ul>
                 {/* Mobile Button */}
-                <div className="black sm:hidden z-10">
+                <div onClick={handleNav} className="black sm:hidden z-10">
                     {nav ? <AiOutlineClose size={20} style={{color: `${textColor}`}} /> : <AiOutlineMenu size={20} style={{color: `${textColor}`}} />}
                 </div>
                 {/* Mobile Menu */}
-                <div className={nav ? 'sm:hidden absolute top-20 left-100px right-0 bottom-0 flex justify-center w-full h-screen bg-red text-center ease-in duration-300': 'sm:hidden absolute top-20 left-[-100%] right-0 bottom-0 flex justify-center w-full h-screen bg-red text-center ease-in duration-300'}> 
-                    <ul className="w-full mx-5">
+                <div className={nav ? 'sm:hidden absolute top-20 left-100px right-0 bottom-0 flex justify-center  w-full h-screen bg-red text-center ease-in duration-300': 'absolute top-20 left-[-100%] right-0 bottom-0 flex justify-center w-full h-screen bg-red text-center ease-in duration-300'}> 
+                    <ul  className="w-full mx-5">
                         <li className="p-4 bg-slate-700 border border-slate-200 text-slate-200 hover:bg-slate-600">
                             <Link href='/'>Homepage</Link>
                         </li>
